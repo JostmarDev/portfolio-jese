d2da9871955841b7e0de14cef462ca5d2e5d529e
JostmarDev <jostmar.dev@gmail.com>
2023-08-10 19:10:04 -0600

    services

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,18 +8,22 @@ import Shapes from './Shapes';
 const Home = () => {
   return (
     <section className="home container" id="home">
+      
       <div className="intro">
         <img src={Me} alt="" className="home__img" />
         <h1 className="home__name">Jese Jostmar</h1>
         <span className="home__education">I'm a software development engineering student</span>
+
         <HeaderSocials/>
         
 
         <a href="#contact" className="btn">Hire me</a>
         <ScrollDown/>
+        
 
       </div>
       <Shapes />
+      
     </section>
   )
 }
